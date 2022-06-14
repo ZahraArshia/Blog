633ab30d0783ab06f51caceb96753f1a6f85cd91
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-14 22:19:52 +0430

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+  {name: "Tom", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from Mexico.", posts_counter: nil},
+  {name: "Lilly", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from Poland.", posts_counter: nil}
+]),
+
+Post.create!([
+  {users_id: 10, title: 'Hello', text: 'This is my first post'}
+  {users_id: 10, title: 'Hello2', text: 'This is my 2nd post'}
+  {users_id: 10, title: 'Hello3', text: 'This is my 3rd post'}
+  {users_id: 10, title: 'Hello4', text: 'This is my 4th post'}
+]),
+
+Comment.create!([
+  {posts_id: 1, users_id: 10, text: 'comment 1'}
+  {posts_id: 1, users_id: 10, text: 'comment 2'}
+  {posts_id: 1, users_id: 10, text: 'comment 3'}
+  {posts_id: 1, users_id: 10, text: 'comment 4'}
+  {posts_id: 1, users_id: 10, text: 'comment 5'}
+  {posts_id: 1, users_id: 10, text: 'comment 6'}
+])
