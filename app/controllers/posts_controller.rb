a7775545cba234054695ffc70e1bc994db8f4717
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-15 20:05:29 +0430

    update routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,29 @@
 class PostsController < ApplicationController
   def index
-    # @user = current_user
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @author = User.find(params[:user_id])
+    @post = @author.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id)
+    else
+      render :new, alert: 'An error has occurred while creating the post'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'likes/new'
+  # get 'comments/new'
+  # get 'likes/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
   # Defines the root path route ("/")
   # root "articles#index"
