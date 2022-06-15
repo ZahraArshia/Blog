1784a3f67707ce5f18f858f9ffc39c909809fc78
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-15 22:31:12 +0430

    fix linter errors

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get comments_new_url
     assert_response :success
   end
