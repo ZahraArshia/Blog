f53211e8268d21f47590ee87478836f554f0abf2
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-17 01:00:37 +0430

    update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,14 +5,14 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def update_post_counter
     author.increment!(:posts_counter)
   end
 
   def recents_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :desc).includes([:author]).limit(5)
   end
 end
