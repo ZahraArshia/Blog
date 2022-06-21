529588d8510250cce2f5305ddf07d377c8c74295
daudi13 <davouma13@gmail.com>
2022-06-21 11:22:32 +0300

    install devise and customise file according to instructions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'comments/new'
   # get 'likes/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root "home#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
