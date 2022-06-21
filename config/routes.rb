a14cf36f0e346f9fc471d77f683071a13baa768e
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-21 15:11:21 +0430

    Configuring Models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'comments/new'
   # get 'likes/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
