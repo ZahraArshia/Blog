ce5599021b42aa98fcfaea08e1ad3891d1d89e61
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-22 13:44:11 +0430

    add destroy to models and routs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes
   after_save :update_post_counter
 
