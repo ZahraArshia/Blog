8cb2a3192602baee0ff147fb2ffeb077b43d822e
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-23 22:05:12 +0430

    create api controller

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::ApiController < ApplicationController::API
+  include JsonWebToken
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split.last if header
+    begin
+      @decoded = jwt_decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+end
